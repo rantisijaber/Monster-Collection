219404ed34b8ab3a4cff62da3730e235b63f9ae1
Jaber <rantisijaber@gmail.com>
2025-02-24 22:19:46 -0500

    Transform App file from class structure to functional structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,57 @@
-import {ChangeEvent, Component} from "react";
-
+import {useState, useEffect} from "react";
 import React from 'react';
 import './App.css';
 import CardList from "./components/card-list/card-list";
 import SearchBox from "./components/search-box/search-box";
 
-interface AppProps {
-
-}
 interface Monster {
     name: string;
     id: number;
     email: string;
 
 }
-interface AppState {
-    monsters: Monster[];
-    searchField: string;
 
-}
-class App extends Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-      super(props);
+const App = () => {
 
-      this.state = {
-          monsters: [],
-          searchField: ''
+    const [searchField, setSearchField] = useState<string>("");
+    const [monsters, setMonsters] = useState<Monster[]>([]);
+    const [filteredMonsters, setFilteredMonsters] = useState<Monster[]>(monsters);
 
-      };
-  }
-    componentDidMount(): void {
-      fetch('https://jsonplaceholder.typicode.com/users')
-          .then((response) => response.json())
-          .then((monsters: Monster[]) : void => this.setState({monsters: monsters}))
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response : Response) => response.json())
+            .then((monsters: Monster[]) : void => setMonsters(monsters))
+    }, []);
 
-  }
-
-    onSearchChange  = (event: ChangeEvent<HTMLInputElement>): void => {
-            const searchField : string = event.target.value.toLowerCase();
-            this.setState({ searchField: searchField })
-    }
-
-    render() {
-
-        const { monsters, searchField } = this.state;
-        const {onSearchChange} = this;
-        const filteredMonsters: Monster[] = monsters.filter((monster) =>
+    useEffect(() => {
+        const newFilteredMonsters = monsters.filter((monster: Monster) =>
             monster.name.toLowerCase().includes(searchField.toLowerCase())
         );
-        return (
-            <div className="App">
-                <h1 className="app-title">Monster Collection</h1>
-                <SearchBox
-                    className={"search-monsters"}
-                    onChangeHandler={onSearchChange}
-                    placeholder={"Search for Monsters"}
-                />
-                <CardList monsters={filteredMonsters} />
+        setFilteredMonsters(newFilteredMonsters);
+    }, [monsters, searchField]);
 
-            </div>
 
-        );
+    const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const searchFieldString = e.target.value;
+        setSearchField(searchFieldString);
     }
+    return (
+        <div className="App">
+            <h1 className="app-title">Monster Collection</h1>
+            <SearchBox
+                className={"search-monsters"}
+                onChangeHandler={onSearchChange}
+                placeholder={"Search for Monsters"}
+            />
+            <CardList monsters={filteredMonsters} />
+
+        </div>
+        )
+
+
+
 }
 
+
 export default App;
+
